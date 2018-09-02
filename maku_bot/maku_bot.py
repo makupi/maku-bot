1c768b3cd1e8fe9409ccad97cef581fffefc027c
Makupi <makupi@icloud.com>
2018-09-02 23:40:22 +0200

    adaption to use Bot extension
    works so far. Count command still missing.
    Proper formating for .top needed.

--- maku_bot/maku_bot.py
+++ maku_bot/maku_bot.py
@@ -5,7 +5,7 @@ import re
 
 import discord
 from discord import Game
-#from discord.ext.commands import Bot
+from discord.ext.commands import Bot
 
 
 import os
@@ -31,7 +31,8 @@ db = mdb_client.servers
 BOT_PREFIX = ('.')
 TOKEN = data['TOKEN']  # Get at discordapp.com/developers/applications/me
 
-client = discord.Client()
+client = Bot(command_prefix=BOT_PREFIX)
+
 
 async def list_servers():
     await client.wait_until_ready()
@@ -72,6 +73,45 @@ def get_count(server_id, emote):
     else: 
         return None
 
+@client.command(name='toc',
+                description='Converts temperature from F to C',
+                brief=': Converts F to C')
+async def to_c(number :int):
+    content = int(number)
+    ce = (((content - 32) * 5) / 9)
+    string = str(content) + "F is equal to " + str(round(ce, 2)) + "C."
+    await client.say(string)
+
+@client.command(name='tof',
+                description='Converts temperature from C to F',
+                brief=': Converts C to F')
+async def to_f(number :int):
+    content = int(number)
+    fh = (((content * 9) / 5) + 32)
+    string = str(content) + "C is equal to " + str(round(fh, 2)) + "F."
+    await client.say(string)
+
+
+@client.command(name='top',
+                description='Lists the top 5 used emotes.',
+                brief=': List top used emotes.',
+                pass_context=True)
+async def top(context):
+    top_emotes = get_top(context.message.server.id)
+    if top_emotes is None:
+        # figure something out
+        return
+    string = "```"
+    string += "Top 5 most used Emotes: \n\n"
+    # em = discord.Embed(
+    #     title='Top 5 most used Emotes'.upper(), color=0x50bdfe)
+    for key, value in top_emotes.items():
+        #string = string + str(key) + ' [' + str(value) + '] ;; '
+        string += ":"+str(key) + ": \n\t" + str(value) + "\n"
+        #em.add_field(name='', value='', inline=False)
+    string += "```"
+    await client.say(string)
+
 
 @client.event
 async def on_message(message):
@@ -79,6 +119,9 @@ async def on_message(message):
     result = regex.findall(message.content)
     result = [r.replace(':', '') for r in result]
     add_results(message.server.id, result)
+
+    await client.process_commands(message)
+    return
 
     if message.author == client.user:
         return
