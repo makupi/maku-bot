c13320a153b29070c82e6cc7ded89eefe93e45d6
Makupi <makupi@icloud.com>
2018-09-15 00:43:36 +0200

    Adds new units and banana

--- maku_bot/cogs/converter.py
+++ maku_bot/cogs/converter.py
@@ -18,10 +18,12 @@ units = {
     'cm': ureg.centimeter,
     'mm': ureg.millimeter,
     'km': ureg.kilometer,
+    'nmi': ureg.nautical_mile,
     'ft': ureg.foot,
     'in': ureg.inch,
     'yd': ureg.yard,
     'kph': (ureg.kilometer / ureg.hour),
+    'kt': ureg.knot,
     'kps': (ureg.kilometer / ureg.second),
     'mps': (ureg.meter / ureg.second),
     'mph': (ureg.mile / ureg.hour),
@@ -33,7 +35,13 @@ units = {
     'kg': ureg.kilogram,
     'lb': ureg.pound,
     'g': ureg.gram,
-    'oz': ureg.ounce
+    'oz': ureg.ounce,
+    'sv': ureg.sievert,
+    'usv': ureg.microsievert,
+    'gy': ureg.gray,
+    'ugy': ureg.microgray,
+    'rem': ureg.rem,
+    'rads': ureg.rads,
 }
 
 class Converter:
@@ -49,7 +57,7 @@ class Converter:
             return
         qunit = units.get(unit.lower(), None)
         qnew_unit = units.get(new_unit.lower(), None)
-        if unit and new_unit:
+        if all([qunit, qnew_unit]):
             quant = Q_(float(value), qunit)
             try:
                 new_quant = quant.to(qnew_unit)
@@ -61,6 +69,9 @@ class Converter:
                 traceback.print_exc(file=sys.stdout)
                 resp = "Conversion from " + unit + " to " + new_unit + " not possible."
         else:
+            if unit.lower() in ('banana', 'bananas'):
+                if await self.banana_exception(value, qnew_unit):
+                    return
             resp = "Invalid Units."
         await self.bot.say(resp)
     
@@ -73,6 +84,32 @@ class Converter:
             string += "{:4s} - {}\n".format(unit_abr, unit)
         string += "```"
         await self.bot.say(string)
+
+    async def banana_exception(self, value, new_unit):
+        rad_banana = 0.1 * ureg.microgray
+        weight_banana = 118 * ureg.gram
+        length_banana = 8 * ureg.inch
+        val = float(value)
+        new_quant = None
+        try:  # try radiation first
+            qval = val * rad_banana
+            new_quant = qval.to(new_unit)
+        except errors.DimensionalityError as de:
+            try:  # try length next
+                qval = val * length_banana
+                new_quant = qval.to(new_unit)
+            except errors.DimensionalityError as de:
+                try:  # try weight next
+                    qval = val * weight_banana
+                    new_quant = qval.to(new_unit)
+                except errors.DimensionalityError as de:
+                    pass
+        if new_quant:
+            resp = "{:.2f}{} is equal to {:.2f}{}.".format(val, 'bananas', new_quant.magnitude, new_unit)
+            await self.bot.say(resp)
+            return True
+        return False
+
 
 def setup(bot):
     bot.add_cog(Converter(bot))
